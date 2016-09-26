04ae39804cf295dfdda4901591262f3025e569e9
liama482 <liamabbate@hanovernorwichschools.org>
2016-09-26 13:39:00 -0400

    Updated from Brython Server: 9/26/2016 1:39:00 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: Liam A
+Credit: 
 
 Assignment:
 
@@ -25,3 +25,8 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+txt=input("Please enter a string of text (the bigger the better): ")
+print("You entered" txt". Now jumble it:")
+def reverse(b):
+    b.dir *= -1
